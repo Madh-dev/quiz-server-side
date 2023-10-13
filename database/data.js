ea36100104e696b26ade0ad2bf9518231dca06b2
Madh-dev <aleemabdulmuheez@gmail.com>
2023-10-13 08:40:36 +0100

    set questions

--- database/data.js
+++ database/data.js
@@ -1,50 +1,243 @@
 /* eslint-disable import/no-anonymous-default-export */
-export default  [
+export default [
     {
         id: 1,
-        question: "Javascript is an _______ language",
+        question: "A female client is scheduled to receive a heart valve replacement with a porcine valve. which of the folllowing types of transplant is this?",
         options: [
-            'Object-Oriented',
-            'Object-Based',
-            'Procedural',
+            'Allogeneic',
+            'Autologous',
+            'Syngeneic',
+            'Xenogeneic'
         ]
     },
     {
         id: 2,
-        question: "Following methods can be used to display data in some form using Javascript",
+        question: "Which of the following should be used for continous bladder irrigation",
         options: [
-            'document.write()',
-            'console.log()',
-            'window.alert()',
+            'Foley catheter',
+            'Three way Foley catheter',
+            'Foley with inflated bag',
+            'Caude catheter'
         ]
     },
     {
         id: 3,
-        question: "When an operator value is NULL, the typeof returned by the unary operator is:",
+        question: "The term 'pink puffer' refers to the female client with which of the following conditions?",
         options: [
-            'Boolean',
-            'Undefined',
-            'Object',
+            'Adult respiratory distress syndrome (ARDS)',
+            'Asthma',
+            'Chronic obstructive bronchitis',
+            'Emphysema'
         ]
     },
     {
         id: 4,
-        question: "What does the toString() method return?",
+        question: "The term 'blue bloater' refers to a male client which of the following conditions?",
         options: [
-            'Return Object',
-            'Return String',
-            'Return Integer'
+            'Adult respiratory distress syndrome (ARDS)',
+            'Asthma',
+            'Chronic obstructive bronchitis',
+            'Emphysema'
         ]
     },
     {
         id: 5,
-        question: "Which function is used to serialize an object into a JSON string?",
+        question: "Circulus arteriosus is an example of anastomotic artery",
         options: [
-            'stringify()',
-            'parse()',
-            'convert()',
+            'True',
+            'False',
         ]
-    }
+    },
+    {
+        id: 6,
+        question: "A murmur is heard at the second left intercostal space along the left sternal border. Which valve area is this?",
+        options: [
+            'Aortic',
+            'Mitra',
+            'Pulmonic',
+            'Tricuspid'
+        ]
+    },
+    {
+        id: 7,
+        question: "______ is designed to carry equipment and materials need during visit at home, school etc",
+        options: [
+            'Home nursing bag',
+            'Public health nursing bag',
+            'Community health nursing bag',
+            'First aid bag'
+        ]
+    },
+    {
+        id: 8,
+        question: "The second stage in family life cycle is ________",
+        options: [
+            'Families with pre school children',
+            'Early child bearing',
+            'Marriage',
+            'Meeting'
+        ]
+    },
+    {
+        id: 10,
+        question: "Which of the following is not true about home visiting?",
+        options: [
+            'The public health nurse should pack near the home if going in a car',
+            'It should not be conducted at night',
+            'The public health nurse should dress moderately',
+            'It should trace defaulters of health care',
+        ]
+    },
+    {
+        id: 11,
+        question: "Which of the following phases of home visit include interaction with the family?",
+        options: [
+            'Contacting phase',
+            'Termination phase',
+            'Entry phase',
+            'Gaining phase'
+        ]
+    }, 
+    {
+        id: 12,
+        question: "In administration of vaccine following the immunization schedule, long time interval up to/more than a year will warrant",
+        options: [
+            'Starting the vaccine over again',
+            'Discontinuation of the vaccine',
+            'Continuation from the previous dose',
+            'None of the above'
+        ]
+    }, 
+    {
+        id: 13,
+        question: ". A variable that can be manipulated by the researcher  is known as  _____",
+        options: [
+            'Independent ',
+            'Dependent ',
+            'Intervening',
+            'Interfering'
+        ]
+    },
+    {
+        id: 14,
+        question: "The hypothesis which occurs when there is change in one variable resulting in change in another variable is _____",
+        options: [
+            'Alternate ',
+            'Complex ',
+            'Associate',
+            'Causal'
+        ]
+    },
+    {
+        id: 15,
+        question: "The hypothesis that shows relationship between variables is _____",
+        options: [
+            'Null ',
+            'Alternate ',
+            'Causal',
+            'Associate'
+        ]
+    },
+    {
+        id: 16,
+        question: "The type of error that rejects a true alternate hypothesis is ____",
+        options: [
+            'Type I error',
+            'Type II error ',
+            'Type I  and II error',
+            'None of the above'
+        ]
+    },
+    {
+        id: 17,
+        question: "The specific individual participating in a case study is ____",
+        options: [
+            'Participant ',
+            'Population ',
+            'Sample',
+            'Subject'
+        ]
+    },
+    {
+        id: 18,
+        question: "The following except ___ are features of true experiment",
+        options: [
+            'Manipulation ',
+            'Randomization ',
+            'Controlling',
+            'Assigning'
+        ]
+    },
+    {
+        id: 19,
+        question: ". ____ theory of leadership is the ability to have some traits in order to succeed.",
+        options: [
+            'Personality ',
+            'Traits ',
+            'Discipline',
+            'Charismatic'
+        ]
+    }, 
+    {
+        id: 20,
+        question: ". ____ is the type of leadership that switches between autocratic and democratic style",
+        options: [
+            'Pseudoautocratic ',
+            'Pseudodemocratic ',
+            'Laissez fair',
+            'Democratic'
+        ]
+    },
+    {
+        id: 21,
+        question: "_____ proposed two factors theory ",
+        options: [
+            'Henri fayol ',
+            'Peter drucker',
+            'Fredrick herzberg',
+            'Victor vroom'
+        ]
+    },
+    {
+        id: 22,
+        question: "Flippo grouped operative functions of human resources management into the following except",
+        options: [
+            'Maintenance',
+            'Separation',
+            'Development',
+            'Resignation'
+        ]
+    },
+    {
+        id: 23,
+        question: "____ is a process of guiding the workers to achieve identified goals",
+        options: [
+            'Supervising',
+            'Directing',
+            'Coordinating',
+            'Controlling'
+        ]
+    },
+    {
+        id: 24,
+        question: "____ is a statement of official rules and regulations guiding organizational operations",
+        options: [
+            'Policy',
+            'Objectives',
+            'Rules',
+            "Do’s and Don’ts"
+        ]
+    },
+    {
+        id: 25,
+        question: "The act of projecting into the future is done in which elements of management",
+        options: [
+            'Staffing',
+            'Controlling',
+            'Budgetting',
+            'Planning'
+        ]
+    },
 ]
 
-export const answers =  [0,1,2,1,0]
+export const answers = [3,1,3,2,0,2,1,1,1,0,2,2,0,2,1,1,3,3,1,1,2,3,3,0,3]
